317daa47bbc086cf3c370d63ade617bf9e0fd8ea
Mohit Malhotra <mohitsco30@gmail.com>
2025-07-12 23:06:40 +0530

    Arithmetic Operator 1

---
+++ src/testpackage1/ArithmaticOpr1.java
@@ -0,0 +1,16 @@
+package testpackage1;
+
+public class ArithmaticOpr1 {
+
+	public static void main(String[] args) {
+		int a = 10; // local var
+		int b = 20; // local var2
+
+		System.out.println(a + b);
+		System.out.println(b - a);
+		System.out.println(a * b);
+		System.out.println(b / a);
+
+	}
+
+}
