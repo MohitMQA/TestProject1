d74c79382d8f53bbfb0989ef3e65d2ddc81644ce
Mohit Malhotra <mohitsco30@gmail.com>
2025-07-13 21:02:49 +0530

    Assignment opr code commit 4

---
+++ src/testpackage1/AssignmentOperator1.java
@@ -0,0 +1,27 @@
+package testpackage1;
+
+public class AssignmentOperator1 {
+	
+	public static void main(String[] args) {
+		
+		int a =10;
+		System.out.println(a+1); //11
+		System.out.println(1+a); //11
+		System.out.println(a); //10
+		System.out.println(a+=3); //13
+		System.out.println(a==3); //false
+		System.out.println(a>3); //true
+		System.out.println(a<3); //false
+		System.out.println(a<=+3); //false
+		System.out.println(a); //13
+		System.out.println(a>=+3); //true
+		System.out.println(a); //13
+		System.out.println(a+=3); //16
+		System.out.println(a-=3); //13
+		System.out.println(a*=3); //39
+		System.out.println(a/=3); //13
+		System.out.println(a); //13
+		
+	}
+
+}
