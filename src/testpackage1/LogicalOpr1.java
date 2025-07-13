9e54a3b8a2edbed819356b11e3d19a1ff25efab2
Mohit Malhotra <mohitsco30@gmail.com>
2025-07-13 22:22:55 +0530

    Bitwsie logial operator1

---
+++ src/testpackage1/LogicalOpr1.java
@@ -0,0 +1,18 @@
+package testpackage1;
+
+public class LogicalOpr1 {
+	
+	public static void main(String[] args) {
+		
+		int a=10;
+		int b=20;
+		int c=30;
+		//bitwise check all condition
+		System.out.println(a<b & a<c); //true
+		System.out.println(a<b & b<c); //true
+		System.out.println(a<b & a<c); //true
+		System.out.println();
+		
+	}
+
+}
