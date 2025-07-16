e7462ae890432d7c85d69fc159ae837ac8898e0b
unknown <mohitmalhotra310@gmail.com>
2025-07-16 16:23:57 +0530

    minor updates...spaces...

--- src/testpackage1/ArithmaticOpr1.java
+++ src/testpackage1/ArithmaticOpr1.java
@@ -5,7 +5,7 @@ public class ArithmaticOpr1 {
 	public static void main(String[] args) {
 		int a = 10; // local var
 		int b = 20; // local var2
-		int c=30;
+		int c=30; // local var
 
 		System.out.println(a + b);
 		System.out.println(b - a);
