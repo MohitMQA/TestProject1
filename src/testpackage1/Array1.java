d7d8ecfba0080371883fe03ec2554f761c81cdc6
unknown <mohitmalhotra310@gmail.com>
2025-07-19 18:55:44 +0530

    array 1

---
+++ src/testpackage1/Array1.java
@@ -0,0 +1,14 @@
+package testpackage1;
+
+public class Array1 {
+
+	public static void main(String[] args) {
+		int[] a = { 1, 2, 3, 4, 5 }; // static array with fixed size
+
+		for (int i = 0; i < a.length; i++) {
+			System.out.println(a[i]);
+		}
+
+	}
+
+}
