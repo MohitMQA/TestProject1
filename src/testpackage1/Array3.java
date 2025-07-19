6f77ffc4eb164b5e042c8776e2560bdd343fca98
unknown <mohitmalhotra310@gmail.com>
2025-07-19 19:18:55 +0530

    array 3 example, printing names and using if loop

---
+++ src/testpackage1/Array3.java
@@ -0,0 +1,28 @@
+package testpackage1;
+
+import java.io.Console;
+
+public class Array3 {
+	
+	public static void main(String[] args) {
+		String[] name = {"mohit", "arjun","sandeep"};
+		
+		//System.out.println(name.length);
+		
+		//System.out.println(name[0]);
+		
+		for(String names : name) {
+//			System.out.println(names);
+			if(names == "arjun") {
+				//System.out.println(names);
+				System.out.println(names);
+				break;
+			}
+			//System.out.println("Session came out of if loop");
+			}
+//		System.out.println("Session came out of for loop");
+			//System.out.println(names[0]);
+		}
+	}
+
+
